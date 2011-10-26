1b4b0e6da615ff7308f5c7fa810c31c1e67fb9d5
Ryan C. Gordon <icculus@icculus.org>
2011-10-26 14:16:41 -0700

    Support HTTP redirects (reddit.com tends to 302 to www.reddit.com).

--- feed/index.php
+++ feed/index.php
@@ -113,6 +113,8 @@ function recache($subreddit, $fname, $url)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
     $xmldata = curl_exec($ch);
     curl_close($ch);
     if ($xmldata === false)
